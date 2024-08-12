7d0c094bb7233b5b86b92807b5368104f680050a
Bob Bui <thang.bui@covestro.com>
2024-08-12 00:53:52 -0700

    test

--- lib/cdk-sam-example-stack.ts
+++ lib/cdk-sam-example-stack.ts
@@ -1,16 +1,15 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
-export class CdkSamExampleStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CdkSamExampleStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkSamExampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new lambda.Function(this, 'MyFunction', {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      handler: 'app.lambda_handler',
+      code: lambda.Code.fromAsset('./my_function'),
+    });
   }
 }
